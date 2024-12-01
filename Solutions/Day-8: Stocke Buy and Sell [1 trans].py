35876433f78947e6ba8552672a53f46094087ed8
mr-mudgal <100049039+mr-mudgal@users.noreply.github.com>
2024-12-01 19:58:21 +0530

    Day-8 Solved

---
+++ Solutions/Day-8: Stocke Buy and Sell [1 trans].py
@@ -0,0 +1,36 @@
+class Solution:
+    def maximumProfit(self, prices):
+        if len(prices) < 2:
+            return 0
+
+        min_price = float('inf')
+        max_profit = 0
+
+        for price in prices:
+            if price < min_price:
+                min_price = price
+
+            profit = price - min_price
+
+            if profit > max_profit:
+                max_profit = profit
+
+        return max_profit
+
+
+#{ 
+ # Driver Code Starts
+if __name__ == "__main__":
+    t = int(input())  # Read number of test cases
+    for _ in range(t):
+        # Read input and split it into a list of integers
+        prices = list(map(int, input().split()))
+        # Create a Solution object and calculate the result
+        obj = Solution()
+        result = obj.maximumProfit(prices)
+        # Print the result
+        print(result)
+
+# } Driver Code Ends
+
+# ⎬coded by⎨ ⌘‣ Mr. Mudgal
